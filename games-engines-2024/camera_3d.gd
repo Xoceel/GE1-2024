ac319ef8d5efa634a877ae39b937dc7417fbbbd4
leeCox <14360547+leecox@user.noreply.gitee.com>
2024-09-27 17:27:16 +0100

    PI does not make a tower

--- games-engines-2024/camera_3d.gd
+++ games-engines-2024/camera_3d.gd
@@ -1,7 +1,7 @@
-extends MeshInstance3D
+extends Camera3D
 
-@export var speed : float = .01
-@export var rot_speed = 180
+var speed = 1
+var rot_speed = 0.8
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,13 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#position.z -= speed + delta
 	var f = Input.get_axis("move_back", "move_forward")
+	position.z += speed * delta * f
 	var r = Input.get_axis("turn_left", "turn_right")
-	print(f)
-	translate(Vector3(0, 0, f * delta * speed))
 	rotate_y(rot_speed * r * delta)
-	#rotate_y(deg_to_rad(rot_speed) * delta)
-	#rotate_x(deg_to_rad(rot_speed) * delta)
-	#rotate_z(deg_to_rad(rot_speed) * delta)
-	
---
+++ games-engines-2024/node_3d.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var brick_scene:PackedScene
+@export var rows = 10
+@export var cols = 100
+
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+	for row in range(rows):
+		for col in range(cols):
+			var my_random_number = rng.randf_range(-0.5, 0.5)
+			var brick = brick_scene.instantiate()
+			var pos = Vector3(col + my_random_number,row - my_random_number, col * PI)
+			brick.position = pos
+			add_child(brick)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
