4d944d2123073bc51c0887ad2f105d3b46a195ef
leeCox <14360547+leecox@user.noreply.gitee.com>
2024-11-15 14:41:19 +0000

    pulled main

--- games-engines-2024/terrain_tile.gd
+++ games-engines-2024/terrain_tile.gd
@@ -54,7 +54,7 @@ func create_mesh():
 	var st = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
-	var bottom_left = Vector3(-quads_per_tile / 2.0, 0, -quads_per_tile / 2.0)
+	var bottom_left = Vector3(-quads_per_tile * width_scale / 2.0, 0, -quads_per_tile * width_scale / 2.0)
 	
 	# Generate vertices and triangles
 	for row in range(quads_per_tile):
